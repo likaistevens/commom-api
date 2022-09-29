56bd9cc6c517e69fa3d0e476acab3de90f80faf3
likai <likai.78@bytedance.com>
2022-09-29 12:37:16 +0800

    fix: 存储目录

--- config.ts
+++ config.ts
@@ -3,5 +3,5 @@ import path from "path";
 export const CONFIG = {
   PORT: parseInt(process.env.PORT || "9500", 10),
   API_PREFIX: "/api/common_api/v1",
-  imageDir: path.join(__dirname, "../../statics/assets/image"),
+  imageDir: path.join(__dirname, "../statics/assets/image"),
 };
